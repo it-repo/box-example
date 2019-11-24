9768fb4810354a06511f79c16f99389e22649c5c
ddosakura <moyinzi@outlook.com>
2019-11-24 17:33:21 +0800

    test sola's file writer

--- sola-example/native/main.go
+++ sola-example/native/main.go
@@ -27,12 +27,28 @@ func main() {
 	fileserver := http.FileServer(httpFs.Dir("public"))
 
 	app := sola.New()
+	app.Dev()
 	r := router.New()
 
 	r.Bind("/public", native.Static("public", "/public"))
 	r.BindFunc("/native", native.From(http.StripPrefix("/native", http.FileServer(http.Dir("public")))))
 	r.BindFunc("/statik", native.From(http.StripPrefix("/statik", http.FileServer(statikFS))))
 	r.BindFunc("/afero", native.From(http.StripPrefix("/afero", fileserver)))
+
+	r.BindFunc("/f", func(c sola.Context) (e error) {
+		var f sola.File
+		if f, e = bp.Open("/main.go"); e != nil {
+			return
+		}
+		return c.File(f)
+	})
+	r.BindFunc("/f2", func(c sola.Context) (e error) {
+		var f sola.File
+		if f, e = statikFS.Open("/sub/index.html"); e != nil {
+			return
+		}
+		return c.File(f)
+	})
 
 	app.Use(r.Routes())
 	sola.Listen("127.0.0.1:3000", app)
