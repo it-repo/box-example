d658d6f5820938bdc46c9a41697407e70957b953
ddosakura <moyinzi@outlook.com>
2019-12-06 15:00:51 +0800

    sola 热更新

---
+++ sola-example/hot-update/README.md
@@ -0,0 +1,4 @@
+# Hot Update
+
+执行 `sola-hot` 进行热更新。
+运行过程中将生成临时可执行文件 `sola-dev`。
---
+++ sola-example/hot-update/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/ddosakura/sola/v2"
+	"github.com/ddosakura/sola/v2/middleware/router"
+)
+
+func main() {
+	app := sola.New()
+	r := router.New(&router.Option{
+		UseNotFound: true,
+	})
+
+	r.Bind("/:t", func(c sola.Context) error {
+		t := router.Param(c, "t")
+		n, _ := strconv.Atoi(t)
+		time.Sleep(time.Duration(n * int(time.Second)))
+		return c.String(http.StatusOK, "sleep "+t)
+	})
+
+	app.Use(r.Routes())
+	sola.Listen("127.0.0.1:3000", app)
+	sola.Keep()
+}
